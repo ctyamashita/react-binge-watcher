31879089d55c4b5c643d4b63903e2da91d9b7435
Celso Yamashita <ctyamashita@hotmail.com>
2022-06-17 14:09:59 +0900

    removing form div

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -14,14 +14,14 @@ const SearchMovies = (props) => {
   }
 
   return (
-    <form action="#" method='get' onSubmit={handleChange}>
+    // <form action="#" method='get' onSubmit={handleChange}>
       <div className="input-group my-5">
         <input type="text" className="form-control" placeholder="Search movies and series by title" aria-label="Search movies and series" aria-describedby="basic-addon2" id="search"/>
         <div className="input-group-append">
-          <button className="btn btn-warning" type="submit"><i className="fas fa-search"></i></button>
+          <button onClick={handleChange} className="btn btn-warning" type="submit"><i className="fas fa-search"></i></button>
         </div>
       </div>
-    </form>
+    // </form>
   );
 };
 
